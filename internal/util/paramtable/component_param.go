be72c08f151a076899f88421310e0e28ca04f4c0
MrPresent-Han <jamesharden11122@gmail.com>
2023-01-14 14:03:50 +0800

    enable parameter for compaction parallel

--- internal/util/paramtable/component_param.go
+++ internal/util/paramtable/component_param.go
@@ -1265,6 +1265,7 @@ type dataCoordConfig struct {
 	SingleCompactionExpiredLogMaxSize int64
 	SingleCompactionDeltalogMaxNum    int64
 	GlobalCompactionInterval          time.Duration
+	GlobalCompactionNodeParallel      int
 
 	// Garbage Collection
 	EnableGarbageCollection bool
@@ -1301,6 +1302,7 @@ func (p *dataCoordConfig) init(base *BaseTable) {
 	p.initSingleCompactionExpiredLogMaxSize()
 	p.initSingleCompactionDeltalogMaxNum()
 	p.initGlobalCompactionInterval()
+	p.initGlobalCompactionParallel()
 
 	p.initEnableGarbageCollection()
 	p.initGCInterval()
@@ -1390,6 +1392,10 @@ func (p *dataCoordConfig) initCompactionTimeoutInSeconds() {
 
 func (p *dataCoordConfig) initCompactionCheckIntervalInSeconds() {
 	p.CompactionCheckIntervalInSeconds = p.Base.ParseInt64WithDefault("dataCoord.compaction.check.interval", 10)
+}
+
+func (p *dataCoordConfig) initGlobalCompactionParallel() {
+	p.GlobalCompactionNodeParallel = p.Base.ParseIntWithDefault("dataCoord.compaction.node.parallel", 2)
 }
 
 // if total delete entities is large than a ratio of total entities, trigger single compaction.
