63dc1997c3843333a98afcc7e5b5af1b328ae60f
MrPresent-Han <jamesharden11122@gmail.com>
2023-01-30 08:18:00 +0800

    lift timeout value for reqContext

--- internal/querynode/shard_cluster.go
+++ internal/querynode/shard_cluster.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+	"time"
 
 	"go.uber.org/atomic"
 	"go.uber.org/zap"
@@ -983,7 +984,7 @@ func (sc *ShardCluster) Search(ctx context.Context, req *querypb.SearchRequest, 
 			queryNode := GetQueryNode()
 			var partialResult *internalpb.SearchResults
 			var nodeErr error
-
+			context.WithTimeout(reqCtx, 7200*time.Second)
 			if queryNode != nil && queryNode.IsStandAlone {
 				partialResult, nodeErr = queryNode.Search(reqCtx, nodeReq)
 			} else {
