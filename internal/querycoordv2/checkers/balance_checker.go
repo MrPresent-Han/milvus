98814915711381778bbc64942b346c033d182b9d
MrPresent-Han <jamesharden11122@gmail.com>
2023-04-15 12:24:25 +0800

    refine balance logic

--- internal/querycoordv2/checkers/balance_checker.go
+++ internal/querycoordv2/checkers/balance_checker.go
@@ -19,20 +19,34 @@ package checkers
 import (
 	"context"
 
+	"github.com/milvus-io/milvus/internal/log"
+	"github.com/milvus-io/milvus/internal/proto/querypb"
 	"github.com/milvus-io/milvus/internal/querycoordv2/balance"
+	"github.com/milvus-io/milvus/internal/querycoordv2/meta"
 	. "github.com/milvus-io/milvus/internal/querycoordv2/params"
+	"github.com/milvus-io/milvus/internal/querycoordv2/session"
 	"github.com/milvus-io/milvus/internal/querycoordv2/task"
+	"github.com/milvus-io/milvus/internal/util/typeutil"
+
+	"github.com/samber/lo"
+	"go.uber.org/zap"
 )
 
 // BalanceChecker checks the cluster distribution and generates balance tasks.
 type BalanceChecker struct {
 	baseChecker
 	balance.Balance
+	meta                            *meta.Meta
+	nodeManager                     *session.NodeManager
+	balancedCollectionsCurrentRound typeutil.UniqueSet
 }
 
-func NewBalanceChecker(balancer balance.Balance) *BalanceChecker {
+func NewBalanceChecker(meta *meta.Meta, balancer balance.Balance, nodeMgr *session.NodeManager) *BalanceChecker {
 	return &BalanceChecker{
-		Balance: balancer,
+		Balance:                         balancer,
+		meta:                            meta,
+		nodeManager:                     nodeMgr,
+		balancedCollectionsCurrentRound: typeutil.NewUniqueSet(),
 	}
 }
 
@@ -40,12 +54,83 @@ func (b *BalanceChecker) Description() string {
 	return "BalanceChecker checks the cluster distribution and generates balance tasks"
 }
 
+func (b *BalanceChecker) collectionsToBalance() []int64 {
+	ids := b.meta.GetAll()
+
+	// loading collection should skip balance
+	loadedCollections := lo.Filter(ids, func(cid int64, _ int) bool {
+		return b.meta.GetCollection(cid).Status == querypb.LoadStatus_Loaded
+	})
+
+	// balance collections influenced by stopping nodes
+	stoppingCollections := make([]int64, 0)
+	for _, cid := range loadedCollections {
+		replicas := b.meta.ReplicaManager.GetByCollection(cid)
+	outer:
+		for _, replica := range replicas {
+			for _, nodeID := range replica.GetNodes() {
+				isStopping, _ := b.nodeManager.IsStoppingNode(nodeID)
+				if isStopping {
+					stoppingCollections = append(stoppingCollections, cid)
+					break outer
+				}
+			}
+		}
+	}
+	//do stopping balance only in this round
+	if len(stoppingCollections) > 0 {
+		return stoppingCollections
+	}
+	//no stopping balance and auto balance is disabled, return empty collections for balance
+	if !Params.QueryCoordCfg.AutoBalance {
+		return make([]int64, 0)
+	}
+	//iterator one normal collection in one round
+	normalCollectionsToBalance := make([]int64, 0)
+	hasUnBalancedCollections := false
+	for _, cid := range loadedCollections {
+		if b.balancedCollectionsCurrentRound.Contain(cid) {
+			log.Debug("ScoreBasedBalancer has balanced collection, skip balancing in this round",
+				zap.Int64("collectionID", cid))
+			continue
+		}
+		hasUnBalancedCollections = true
+		normalCollectionsToBalance = append(normalCollectionsToBalance, cid)
+		b.balancedCollectionsCurrentRound.Insert(cid)
+		break
+	}
+
+	if !hasUnBalancedCollections {
+		b.balancedCollectionsCurrentRound.Clear()
+		log.Debug("ScoreBasedBalancer has balanced all " +
+			"collections in one round, clear collectionIDs for this round")
+	}
+	return normalCollectionsToBalance
+}
+
+func (b *BalanceChecker) balanceCollections(collectionsToBalance []int64) ([]balance.SegmentAssignPlan, []balance.ChannelAssignPlan) {
+	segmentPlans, channelPlans := make([]balance.SegmentAssignPlan, 0), make([]balance.ChannelAssignPlan, 0)
+	for _, cid := range collectionsToBalance {
+		replicas := b.meta.ReplicaManager.GetByCollection(cid)
+		for _, replica := range replicas {
+			sPlans, cPlans := b.Balance.BalanceReplica(replica)
+			balance.PrintNewBalancePlans(cid, replica.GetID(), sPlans, cPlans)
+			segmentPlans = append(segmentPlans, sPlans...)
+			channelPlans = append(channelPlans, cPlans...)
+		}
+		if len(segmentPlans) != 0 || len(channelPlans) != 0 {
+			log.Debug("BalanceChecker has generated balance plans for", zap.Int64("collectionID", cid))
+			break
+		}
+	}
+	return segmentPlans, channelPlans
+}
+
 func (b *BalanceChecker) Check(ctx context.Context) []task.Task {
 	ret := make([]task.Task, 0)
-	if !Params.QueryCoordCfg.AutoBalance {
-		return ret
-	}
-	segmentPlans, channelPlans := b.Balance.Balance()
+
+	collectionsToBalance := b.collectionsToBalance()
+	segmentPlans, channelPlans := b.balanceCollections(collectionsToBalance)
 
 	tasks := balance.CreateSegmentTasksFromPlans(ctx, b.ID(), Params.QueryCoordCfg.SegmentTaskTimeout, segmentPlans)
 	task.SetPriority(task.TaskPriorityLow, tasks...)
