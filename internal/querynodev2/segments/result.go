d1077e4b01566240951b3d4db5dc9da91f9a337b
MrPresent-Han <chun.han@zilliz.com>
2023-10-20 16:37:26 +0800

    enable query reduce on querynodes for shortcut return
    Signed-off-by: MrPresent-Han <chun.han@zilliz.com>

--- internal/querynodev2/segments/result.go
+++ internal/querynodev2/segments/result.go
@@ -245,6 +245,10 @@ func MergeInternalRetrieveResult(ctx context.Context, retrieveResults []*interna
 		zap.Int64("limit", param.limit),
 		zap.Int("resultNum", len(retrieveResults)),
 	)
+	if len(retrieveResults) == 1 {
+		log.Debug("MergeInternalRetrieveResult is single result, shortcut return")
+		return retrieveResults[0], nil
+	}
 	var (
 		ret = &internalpb.RetrieveResults{
 			Status: merr.Success(),
