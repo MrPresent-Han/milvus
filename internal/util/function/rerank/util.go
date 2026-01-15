2db83b75dd5528e48eaa983a1ed1b55e583bc060
groot <yihua.mo@zilliz.com>
2026-01-15 11:57:28 +0800

    enhance: allow alter_collection_field() to change field description (#47057)
    issue: https://github.com/milvus-io/milvus/discussions/46896
    
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- internal/util/function/rerank/util.go
+++ internal/util/function/rerank/util.go
@@ -340,7 +340,12 @@ func newGroupingIDScores[T PKType](idScores map[T]float32, idLocations map[T]IDL
 		0,
 		make([]IDLoc, 0, searchParams.limit),
 	}
-	for index := int(searchParams.offset); index < len(groupList); index++ {
+	// Ensure we only output limit groups starting from offset
+	endIndex := int(searchParams.offset) + int(searchParams.limit)
+	if endIndex > len(groupList) {
+		endIndex = len(groupList)
+	}
+	for index := int(searchParams.offset); index < endIndex; index++ {
 		group := groupList[index]
 		for i, score := range group.scoreList {
 			// idList and scoreList must have same length
